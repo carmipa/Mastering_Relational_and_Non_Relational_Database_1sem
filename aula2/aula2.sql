fa3ebe34c78b189d963b99230b52901c04af68cb
carmipa <pabcarminati@gmail.com>
2025-02-20 11:57:31 -0300

    aula2

--- aula2/aula2.sql
+++ aula2/aula2.sql
@@ -3,7 +3,7 @@ SET SERVEROUTPUT ON;
 DECLARE
     v_nome emp.ename%TYPE := 'Paulo';
     v_sal number(7,2):= 100;
-    v_sal2  v_sal%TYPE; -- variavel 2 é do mesmo tipo de dado da variavel 1
+    v_sal2  v_sal%TYPE; -- variavel 2 Ã© do mesmo tipo de dado da variavel 1
     v_bol boolean;
 BEGIN
     v_bol := 1 > 2;
