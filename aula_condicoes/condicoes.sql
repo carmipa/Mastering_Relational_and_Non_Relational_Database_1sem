b9c0362ca7e1cea60decbf0e22edb41850a9385a
carmipa <pabcarminati@gmail.com>
2025-03-25 20:51:41 -0300

    aulas

--- aula_condicoes/condicoes.sql
+++ aula_condicoes/condicoes.sql
@@ -78,9 +78,32 @@ ROLLBACK;
 
 
 
+SELECT * FROM EMP;
+ROLLBACK;
 
 
 
+SET SERVEROUTPUT ON;
+
+
+
+DECLARE
+    v_YEAR NUMBER(4) := &P_YEAR;
+    v_REMAINDER1 NUMBER(5,2);
+    v_REMAINDER2 NUMBER(5,2);
+    v_REMAINDER3 NUMBER(5,2);
+BEGIN
+    v_REMAINDER1 := MOD(v_YEAR,4);
+    v_REMAINDER2 := MOD(v_YEAR,100);
+    v_REMAINDER3 := MOD(v_YEAR,400);
+    IF ((v_REMAINDER1 = 0 AND v_REMAINDER2 <> 0 ) OR
+        v_REMAINDER3 = 0) THEN
+        DBMS_OUTPUT.PUT_LINE(v_YEAR || ' é bissexto');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE(v_YEAR || ' não é bissexto');
+    END IF;
+END;
+/
 
 
 
