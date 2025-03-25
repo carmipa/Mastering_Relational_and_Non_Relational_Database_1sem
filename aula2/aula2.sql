f0b1502f33f5b9931a12bed4886fd492f7d88b1e
carmipa <pabcarminati@gmail.com>
2025-03-25 20:27:24 -0300

    aula loop e condições

--- aula2/aula2.sql
+++ aula2/aula2.sql
@@ -1,15 +1,4 @@
-SET SERVEROUTPUT ON;
 
-DECLARE
-    v_nome emp.ename%TYPE := 'Paulo';
-    v_sal number(7,2):= 100;
-    v_sal2  v_sal%TYPE; -- variavel 2 é do mesmo tipo de dado da variavel 1
-    v_bol boolean;
-BEGIN
-    v_bol := 1 > 2;
-    select ename, sal into v_nome, v_sal
-        from emp
-    WHERE sal > 4000;
-    DBMS_OUTPUT.PUT_LINE(v_nome || '' || TO_CHAR(v_sal, '$9G999'));
-END;
-/
+
+
+show recyclebin;
---
+++ aula_condicoes/condicoes.sql
@@ -0,0 +1,106 @@
+SET SERVEROUTPUT ON
+DECLARE 
+    v_media emp.sal%TYPE := 0;
+BEGIN
+    SELECT AVG(sal) INTO v_media
+        FROM emp;
+    IF v_media < 3000 THEN
+        DBMS_OUTPUT.PUT_LINE ('Não trabalhe aqui');
+    ELSIF v_media < 3000 THEN
+         DBMS_OUTPUT.PUT_LINE ('Que salário horrível');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE ('Hora depedir aumento');
+    end if;
+end;
+/
+
+
+SET SERVEROUTPUT ON
+DECLARE
+    v_opcao number(2) := &Escolha;
+BEGIN
+    case v_opcao
+        WHEN 10 THEN DBMS_OUTPUT.PUT_LINE ('você escolheu 10');
+        WHEN 20 THEN DBMS_OUTPUT.PUT_LINE ('você escolheu 20');
+        WHEN 30 THEN DBMS_OUTPUT.PUT_LINE ('você escolheu 30');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE ('opção inválida');
+    end case;
+end;
+/
+
+
+DECLARE
+    V_maior emp.sal%TYPE := 0;
+    V_cont NUMBER := 1;
+BEGIN
+    SELECT MAX(sal) INTO v_maior
+        FROM emp;
+    LOOP
+        INSERT INTO EMP (EMPNO, SAL) VALUES (v_cont, v_maior + v_cont * 100);
+        v_cont := v_cont + 1;
+        EXIT WHEN v_CONT > 4;
+    END LOOP;
+END;
+/
+
+
+DECLARE
+    V_maior emp.sal%TYPE := 0;
+    V_cont NUMBER := 1;
+BEGIN
+    SELECT MAX(sal) INTO v_maior
+        FROM emp;
+    WHILE  vcont < 5 LOOP
+        INSERT INTO EMP (EMPNO, SAL) VALUES (v_cont, v_maior + v_cont * 100);
+        v_cont := v_cont + 1;
+    END LOOP;
+END;
+/
+
+SELECT * FROM EMP;
+ROLLBACK;
+
+
+DECLARE
+    V_maior emp.sal%TYPE := 0;
+    V_cont NUMBER := 1;
+BEGIN
+    SELECT MAX(sal) INTO v_maior
+        FROM emp;
+    FOR i IN 1..4 LOOP
+        INSERT INTO EMP (EMPNO, SAL) VALUES (i, v_maior + i * 100);
+    END LOOP;
+END;
+/
+SELECT * FROM EMP;
+ROLLBACK;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
